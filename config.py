247dd16b7a44336d100a40c11e659e26aea67733
AquariusRH <79887068+AquariusRH@users.noreply.github.com>
2025-09-06 16:40:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ METHOD_LIST_WITH_QPL = ["WIN", "PLA", "QIN", "QPL", "FCT", "TRI", "FF"]
 METHOD_LIST_WITHOUT_QPL = ["WIN", "PLA", "QIN", "TRI"]
 METHOD_CH_WITH_QPL = ["獨贏", "位置", "連贏", "位置Q", "二重彩", "單T", "四連環"]
 METHOD_CH_WITHOUT_QPL = ["獨贏", "位置", "連贏", "單T"]
-PRINT_LIST_WITH_QPL = ["qin_qpl", "PLA", "WIN"]
-PRINT_LIST_WITHOUT_QPL = ["qin", "PLA", "WIN"]
+PRINT_LIST_WITH_QPL = ["QIN","QPL", "PLA", "WIN"]
+PRINT_LIST_WITHOUT_QPL = ["QIN", "PLA", "WIN"]
 
 # 基準值
 BENCHMARK_DICT = {
